cb911facb72da4e0dbbc79e6c7626e5f68e1071d
jerrymei <328047478@qq.com>
2017-07-10 00:28:57 +0800

    添加注释

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 $(function () {
+    //点击向下的箭头滚动到文章区域
     $('.scroll-down').click(function () {
         var scrollTop = $('.site-content').offset().top;
         $('body').animate({scrollTop: scrollTop}, 600);
