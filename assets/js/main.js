33082e8319c01235e4bf61b881c804e08044306b
JerryMei <328047478@qq.com>
2017-07-10 13:49:28 +0800

    新增瀑布流

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,5 +4,16 @@ $(function () {
         var scrollTop = $('.site-content').offset().top;
         $('body').animate({scrollTop: scrollTop}, 600);
     });
+
+    $('.grid').masonry({
+        columnWidth: 'article',
+        itemSelector: 'article',
+        percentPosition: true
+    });
+
+    if ($('body').hasClass('paged')) {
+        var scrollTop = $('.site-content').offset().top;
+        $('body').animate({scrollTop: scrollTop}, 0);
+    }
 });
 
