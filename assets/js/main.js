377d48a334ac3463ebc528afa20526805a01942a
jerrymei <328047478@qq.com>
2017-07-17 23:16:06 +0800

    toc修改

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,20 +21,22 @@ $(function () {
         'container': '.md-preview', //element to find all selectors in
         'smoothScrolling': true, //enable or disable smooth scrolling on click
         'prefix': 'toc', //prefix for anchor tags and class names
-        'onHighlight': function(el) {}, //called when a new section is highlighted
         'highlightOnScroll': true, //add class to heading that is currently in focus
         'highlightOffset': 100, //offset to trigger the next headline
-        'anchorName': function(i, heading, prefix) { //custom function for anchor name
-            return prefix+i;
+        'anchorName': function (i, heading, prefix) { //custom function for anchor name
+            return prefix + i;
         },
-        'headerText': function(i, heading, $heading) { //custom function building the header-item text
+        'headerText': function (i, heading, $heading) { //custom function building the header-item text
             return $heading.text();
         }
     });
 
+    //toc设置
     $(window).scroll(function () {
-        if($(window).scrollTop>100){
-            console.log("aa");
+        if ($(window).scrollTop() > $('header').outerHeight()) {
+            $('#toc').addClass('toc-fixed');
+        } else {
+            $('#toc').removeClass('toc-fixed');
         }
     })
 });
