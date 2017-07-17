eeb3334fc279348249a504a55a7704436c899869
JerryMei <328047478@qq.com>
2017-07-17 18:17:09 +0800

    新增toc

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,5 +15,27 @@ $(function () {
         var scrollTop = $('.site-content').offset().top;
         $('body').animate({scrollTop: scrollTop}, 0);
     }
+
+    $('#toc').toc({
+        'selectors': 'h1,h2,h3', //elements to use as headings
+        'container': '.md-preview', //element to find all selectors in
+        'smoothScrolling': true, //enable or disable smooth scrolling on click
+        'prefix': 'toc', //prefix for anchor tags and class names
+        'onHighlight': function(el) {}, //called when a new section is highlighted
+        'highlightOnScroll': true, //add class to heading that is currently in focus
+        'highlightOffset': 100, //offset to trigger the next headline
+        'anchorName': function(i, heading, prefix) { //custom function for anchor name
+            return prefix+i;
+        },
+        'headerText': function(i, heading, $heading) { //custom function building the header-item text
+            return $heading.text();
+        }
+    });
+
+    $(window).scroll(function () {
+        if($(window).scrollTop>100){
+            console.log("aa");
+        }
+    })
 });
 
