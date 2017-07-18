5f31485640e7606956fd03d252340724d22f2a7e
JerryMei <328047478@qq.com>
2017-07-18 13:49:28 +0800

    修改toc滚动的算法，保持和Bootstrap文档里的toc一样

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,15 +19,10 @@ $(function () {
     $('#toc').toc({
         'selectors': 'h1,h2,h3', //elements to use as headings
         'container': '.md-preview', //element to find all selectors in
-        'smoothScrolling': true, //enable or disable smooth scrolling on click
-        'prefix': 'toc', //prefix for anchor tags and class names
-        'highlightOnScroll': true, //add class to heading that is currently in focus
-        'highlightOffset': 100, //offset to trigger the next headline
+        'prefix': 'toc',
+        'highlightOffset': 0, //offset to trigger the next headline
         'anchorName': function (i, heading, prefix) { //custom function for anchor name
             return prefix + i;
-        },
-        'headerText': function (i, heading, $heading) { //custom function building the header-item text
-            return $heading.text();
         }
     });
 
