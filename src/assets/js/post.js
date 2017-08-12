8b4db83d02064e590c051776988c230de9b47def
jerrymei <328047478@qq.com>
2017-08-12 15:42:12 +0800

    优化toc样式

--- src/assets/js/post.js
+++ src/assets/js/post.js
@@ -1,14 +1,23 @@
 $(function () {
-    //初始化toc
-    $('#toc').toc({
-        'selectors': 'h1,h2,h3', //elements to use as headings
-        'container': '.md-preview', //element to find all selectors in
-        'prefix': 'toc',
-        'highlightOffset': 1, //offset to trigger the next headline
-        'anchorName': function (i, heading, prefix) { //custom function for anchor name
-            return prefix + i;
-        }
-    });
+    //如果文章没有标题不会初始化toc
+    var has_toc = $('.md-preview h1,.md-preview h2,.md-preview h3').length > 0;
+    if (has_toc) {
+        //显示侧边栏
+        $('.navbar-side').removeClass('hidden');
+        $('.article-left').addClass('has-side');
+
+        //初始化toc
+        $('#toc').toc({
+            'selectors': 'h1,h2,h3', //elements to use as headings
+            'container': '.md-preview', //element to find all selectors in
+            'prefix': 'toc',
+            'highlightOffset': 1, //offset to trigger the next headline
+            'anchorName': function (i, heading, prefix) { //custom function for anchor name
+                return prefix + i;
+            }
+        });
+    }
+
 
     //toc滚动设置
     $(window).scroll(function () {
@@ -20,7 +29,7 @@ $(function () {
         } else if (scroll_top > header_height && scroll_top <= max_height) {
             $('#toc').removeClass('toc-absolute').addClass('toc-fixed').removeAttr('style');
         } else {
-            $('#toc').removeClass('toc-fixed').addClass('toc-absolute').css("top", max_height);
+            $('#toc').removeClass('toc-fixed').addClass('toc-absolute').css("top", max_height - $('.md-preview').offset().top);
         }
     });
 
