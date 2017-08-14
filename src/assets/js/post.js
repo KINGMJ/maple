ac938a105f7f98c6ff0df783fe838ebdf3b1ecaa
JerryMei <328047478@qq.com>
2017-08-14 09:50:18 +0800

    链接新页面打开

--- src/assets/js/post.js
+++ src/assets/js/post.js
@@ -63,4 +63,16 @@ $(function () {
         $('#img_wrapper img').attr('src', '');
         $('body').removeAttr('style');
     });
+
+    //为超链接加上target='_blank'属性
+    $(document).bind('DOMNodeInserted', function () {
+        addBlankTargetForLinks();
+    });
 });
+
+
+function addBlankTargetForLinks() {
+    $('.md-preview a[href^="http"]').each(function () {
+        $(this).attr('target', '_blank');
+    });
+}
