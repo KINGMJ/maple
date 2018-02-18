8cf3a83cdc62e6f098e5bd8045876f2f38b4277c
jerrymei <328047478@qq.com>
2018-02-19 00:43:03 +0800

    升级到Ghost v1.x

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # maple
 IT's a theme about ghost
+
+支持v1.x
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -19,9 +19,32 @@ $(function () {
         $('.site-header').removeClass('invisible');
     }
 
-    //显示标签的数量
+    //标签云
+    $.get(
+        ghost.url.api('tags', {
+            limit: 'all',
+            include: 'count.posts',
+            order: 'count.posts DESC'
+        })
+    ).done(onSuccess);
+});
+
+function onSuccess(data) {
+    console.log(data);
+    var html = '';
+    $.each(data.tags, function (i, tag) {
+        var backgroundImage = 'background-image:url(' + tag.feature_image + ')';
+        html += "<a href='/tag/" + tag.slug + "' style='" + backgroundImage + "'>" +
+            "<div class='overlay'><h2>" + tag.name + "</h2><button class='info'>Show</button></div></a>";
+    });
+    $(html).prependTo($('.footer-content-tags'));
+
+    //标签云页面
     if ($('body').hasClass('page-tag_cloud')) {
+        $(html).prependTo($('.tags-container'));
         var tag_count = $('.tags-container a').length;
         $('.tag-post-num .num').text(tag_count);
     }
-});
+
+
+}
