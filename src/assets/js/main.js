a3374f07590721f41188a41fa9f446e61f3c30ac
KINGMJ <328047478@qq.com>
2020-08-24 14:44:55 +0800

    注释掉标签云功能，新版本废弃掉ghost.url.api操作

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -18,34 +18,34 @@ $(function () {
     }
 
     //标签云
-    $.get(
-        ghost.url.api('tags', {
-            limit: 24,
-            include: 'count.posts',
-            order: 'count.posts DESC'
-        })
-    ).done(onSuccess);
+    // $.get(
+    //     ghost.url.api('tags', {
+    //         limit: 24,
+    //         include: 'count.posts',
+    //         order: 'count.posts DESC'
+    //     })
+    // ).done(onSuccess);
 });
 
-function onSuccess(data) {
-    var html = '';
-    $.each(data.tags, function (i, tag) {
-        html += "<a href='/tag/" + tag.slug + "'";
-        if (tag.feature_image !== null) {
-            var backgroundImage = 'background-image:url(' + tag.feature_image + ')';
-            html += "style='" + backgroundImage + "'";
-        }
-        html += "><div class='overlay'><h2>" + tag.name + "</h2><button class='info'>Show</button></div></a>";
-    });
-    $(html).prependTo($('.footer-content-tags'));
-
-    //标签云页面
-    if ($('body').hasClass('page-tag_cloud')) {
-        $(html).prependTo($('.tags-container'));
-        var tag_count = $('.tags-container a').length;
-        $('.tag-post-num .num').text(tag_count);
-    }
-}
+// function onSuccess(data) {
+//     var html = '';
+//     $.each(data.tags, function (i, tag) {
+//         html += "<a href='/tag/" + tag.slug + "'";
+//         if (tag.feature_image !== null) {
+//             var backgroundImage = 'background-image:url(' + tag.feature_image + ')';
+//             html += "style='" + backgroundImage + "'";
+//         }
+//         html += "><div class='overlay'><h2>" + tag.name + "</h2><button class='info'>Show</button></div></a>";
+//     });
+//     $(html).prependTo($('.footer-content-tags'));
+//
+//     //标签云页面
+//     if ($('body').hasClass('page-tag_cloud')) {
+//         $(html).prependTo($('.tags-container'));
+//         var tag_count = $('.tags-container a').length;
+//         $('.tag-post-num .num').text(tag_count);
+//     }
+// }
 
 /**
  * 随机生成博客封面
