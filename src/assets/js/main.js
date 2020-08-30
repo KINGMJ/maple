4845d1a03071fb07d581992f4182bea1abdcf922
KINGMJ <328047478@qq.com>
2020-08-30 16:24:45 +0800

    package.json修改，适配ghost v3

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -16,37 +16,7 @@ $(function () {
         $('html,body').animate({scrollTop: scrollTop}, 0);
         $('.site-header').removeClass('invisible');
     }
-
-    //标签云
-    // $.get(
-    //     ghost.url.api('tags', {
-    //         limit: 24,
-    //         include: 'count.posts',
-    //         order: 'count.posts DESC'
-    //     })
-    // ).done(onSuccess);
 });
-
-// function onSuccess(data) {
-//     var html = '';
-//     $.each(data.tags, function (i, tag) {
-//         html += "<a href='/tag/" + tag.slug + "'";
-//         if (tag.feature_image !== null) {
-//             var backgroundImage = 'background-image:url(' + tag.feature_image + ')';
-//             html += "style='" + backgroundImage + "'";
-//         }
-//         html += "><div class='overlay'><h2>" + tag.name + "</h2><button class='info'>Show</button></div></a>";
-//     });
-//     $(html).prependTo($('.footer-content-tags'));
-//
-//     //标签云页面
-//     if ($('body').hasClass('page-tag_cloud')) {
-//         $(html).prependTo($('.tags-container'));
-//         var tag_count = $('.tags-container a').length;
-//         $('.tag-post-num .num').text(tag_count);
-//     }
-// }
-
 /**
  * 随机生成博客封面
  */
