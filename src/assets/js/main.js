68c4fd69a83ae01740e1af987318b538f4de182d
KINGMJ <328047478@qq.com>
2021-10-08 11:33:47 +0800

    标签页和归档页样式调整

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,38 +1,48 @@
 $(function () {
-    //随机显示博客封面
-    if (Maple.enableRandomCover) {
-        randomSiteCover();
-    }
+  //随机显示博客封面
+  if (Maple.enableRandomCover) {
+    randomSiteCover();
+  }
 
-    //点击向下的箭头滚动到文章区域
-    $('.indicator').click(function () {
-        var scrollTop = $('.site-content').offset().top;
-        $('html,body').animate({ scrollTop: scrollTop }, 500);
-    });
+  //点击向下的箭头滚动到文章区域
+  $(".indicator").click(function () {
+    var scrollTop = $(".site-content").offset().top;
+    $("html,body").animate({ scrollTop: scrollTop }, 500);
+  });
 
-    //默认滚动到文章列表
-    if ($('body').hasClass('paged')) {
-        var scrollTop = $('.site-content').offset().top;
-        $('html,body').animate({ scrollTop: scrollTop }, 0);
-        $('.site-header').removeClass('invisible');
-    }
+  //默认滚动到文章列表
+  if ($("body").hasClass("paged")) {
+    var scrollTop = $(".site-content").offset().top;
+    $("html,body").animate({ scrollTop: scrollTop }, 0);
+    $(".site-header").removeClass("invisible");
+  }
 
-    //标签页面移除最后一个没有内容的标签
-    removeNoContentTag()
+  //标签页面移除最后一个没有内容的标签
+  removeNoContentTag();
 });
 
 /**
  * 随机生成博客封面
  */
 function randomSiteCover() {
-    var randomNum = Math.floor(Math.random() * 30 + 1);
-    $('.site-header').css('background-image', "url(" + Maple.blogUrl + "/assets/images/30+wallpaper/bg-cover" + randomNum + ".jpg)");
+  var randomNum = Math.floor(Math.random() * 30 + 1);
+  $(".site-header").css(
+    "background-image",
+    "url(" +
+      Maple.blogUrl +
+      "/assets/images/30+wallpaper/bg-cover" +
+      randomNum +
+      ".jpg)"
+  );
 }
 
-
 function removeNoContentTag() {
-    var last = $('.tag-posts').children().last()
-    if (!!last[0] && last[0].tagName == "SPAN") {
-        last.remove()
+  var child = $(".tag-posts").children();
+  child.each(function (index, element) {
+    var next = $(element).next();
+    var hasNext = !!next && next.is("ul");
+    if ($(element).is("span") && !hasNext) {
+      $(element).remove();
     }
+  });
 }
